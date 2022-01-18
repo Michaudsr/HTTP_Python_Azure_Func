dd7282b8052c0fe0600ea0a6fb2ecd837fb6601c
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2022-01-17 22:11:43 -0500

    [init] successfully runs get and post requests

--- HTTP_Python/__init__.py
+++ HTTP_Python/__init__.py
@@ -6,19 +6,22 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    method_type = req.method
+    if method_type == "GET":
+        name = req.params.get('name')
+    elif method_type == "POST":
         try:
             req_body = req.get_json()
         except ValueError:
-            pass
+            name = None
         else:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.",
+        status_code=200)
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "Pass a name in the query string (GET request) or a JSON body with the attribute name (POST request) for a personalized response.",
+            status_code=400
         )
